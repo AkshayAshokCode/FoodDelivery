b5f69272b87a848e047937d2520359530f8c206c
akshay ashok <akshayashokan1054@gmail.com>
2024-03-06 08:20:48 +0530

    main food page setup

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({super.key});
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
