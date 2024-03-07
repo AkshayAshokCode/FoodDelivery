1edcb096899b1f4ae5ab1973700a388448f9a197
akshay ashok <akshayashokan1054@gmail.com>
2024-03-07 08:39:21 +0530

    add main food page setup

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -10,6 +12,39 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+       body: Column(
+         children: [
+           Container(
+               child: Container(
+                 margin: EdgeInsets.only(top: 45, bottom: 15),
+                 padding: EdgeInsets.only(left: 20, right: 20),
+                 child: Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                   children: [
+                     Column(
+                       children: [
+                         Text("Country"),
+                         Text("City")
+                       ],
+                     ),
+                     Center(
+                       child: Container(
+                         width: 45,
+                         height: 45,
+                         child: Icon(Icons.search, color: Colors.white),
+                         decoration: BoxDecoration(
+                           borderRadius: BorderRadius.circular(15),
+                           color: AppColors.mainColor,
+                         ),
+                       ),
+                     )
+                   ],
+                 ),
+               )
+           )
+         ],
+       )
+    );
   }
 }
