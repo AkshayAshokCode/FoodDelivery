56a0bd1afe225ea36d7ba36920275b8f26d5ec62
akshay ashok <akshayashokan1054@gmail.com>
2024-04-03 22:55:11 +0530

    add dynamic dimensions

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/home/food_page_body.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/small_text.dart';
+
+import '../utils/dimensions.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -21,8 +22,8 @@ class _MainFoodPageState extends State<MainFoodPage> {
            //showing the header
            Container(
                child: Container(
-                 margin: EdgeInsets.only(top: 45, bottom: 15),
-                 padding: EdgeInsets.only(left: 20, right: 20),
+                 margin: EdgeInsets.only(top: Dimensions.height45, bottom: Dimensions.width15),
+                 padding: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20),
                  child: Row(
                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                    children: [
@@ -40,13 +41,13 @@ class _MainFoodPageState extends State<MainFoodPage> {
                      ),
                      Center(
                        child: Container(
-                         width: 45,
-                         height: 45,
-                         child: Icon(Icons.search, color: Colors.white),
+                         width: Dimensions.width45,
+                         height: Dimensions.height45,
                          decoration: BoxDecoration(
-                           borderRadius: BorderRadius.circular(15),
+                           borderRadius: BorderRadius.circular(Dimensions.radius15),
                            color: AppColors.mainColor,
                          ),
+                         child: Icon(Icons.search, color: Colors.white, size: Dimensions.iconSize24,),
                        ),
                      )
                    ],
--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
+import '../utils/dimensions.dart';
+
 class BigText extends StatelessWidget {
   Color? color;
   final String text;
@@ -7,7 +9,7 @@ class BigText extends StatelessWidget {
   TextOverflow overFlow;
   BigText({super.key, this.color = const Color(0xFF332d2b),
     required this.text,
-    this.size=20,
+    this.size=0,
   this.overFlow=TextOverflow.ellipsis});
 
   @override
@@ -19,7 +21,7 @@ class BigText extends StatelessWidget {
       style: TextStyle(
         fontFamily: 'Roboto',
         color: color,
-        fontSize: size,
+        fontSize: size == 0 ? Dimensions.font20 : size,
         fontWeight: FontWeight.w400)
     );
   }
