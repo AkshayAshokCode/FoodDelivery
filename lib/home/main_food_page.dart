03286ba7ed38ffa8ee0fe9d8591c6d108e057407
akshay ashok <akshayashokan1054@gmail.com>
2024-04-03 22:17:02 +0530

    add dots

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -18,6 +18,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
     return Scaffold(
        body: Column(
          children: [
+           //showing the header
            Container(
                child: Container(
                  margin: EdgeInsets.only(top: 45, bottom: 15),
@@ -52,6 +53,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
                  ),
                )
            ),
+           //showing the body
            FoodPageBody()
          ],
        )
