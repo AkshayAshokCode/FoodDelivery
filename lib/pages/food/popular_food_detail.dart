3ebee80421081eacdfa41f40d61dd07ba085dde6
akshay ashok <akshayashokan1054@gmail.com>
2024-04-09 17:31:07 +0530

    add detail container

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/dimensions.dart';
 import 'package:food_delivery/widgets/app_icon.dart';
 
+import '../../utils/colors.dart';
+import '../../widgets/big_text.dart';
+import '../../widgets/icon_and_text_widget.dart';
+import '../../widgets/small_text.dart';
+
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({super.key});
 
@@ -37,6 +42,52 @@ class PopularFoodDetail extends StatelessWidget {
                 AppIcon(icon: Icons.shopping_cart_outlined)
               ],
 
+        )),
+        Positioned(
+            left:0,
+            right: 0,
+            bottom: 0,
+            top: Dimensions.popularFoodImgSize - 20,
+            child: Container(
+              padding: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20, top: Dimensions.height20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(Dimensions.radius20),
+                  topLeft: Radius.circular(Dimensions.radius20)
+                ),
+                color: Colors.white
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Chinese Side", color: Colors.black54),
+                  SizedBox(height: Dimensions.height10,),
+                  Row(
+                      children: [
+                        Wrap(
+                            children: List.generate(
+                                5, (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15,)
+                            )
+                        ),
+                        SizedBox(width: 10,),
+                        SmallText(text: "4.5"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "1287"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "comments"),
+                      ]
+                  ),
+                  SizedBox(height: Dimensions.height20,),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconAndTextWidget(icon: Icons.circle_sharp, text: "Normal", iconColor: AppColors.iconColor1),
+                        IconAndTextWidget(icon: Icons.location_on, text: "1.7km", iconColor: AppColors.mainColor),
+                        IconAndTextWidget(icon: Icons.access_time_rounded, text: "32min", iconColor: AppColors.iconColor2)
+                      ]
+                  )
+                ],
+              ),
         ))
       ],
     )
