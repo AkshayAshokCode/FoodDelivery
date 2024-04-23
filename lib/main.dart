31e69f2c2b994b147a97758ac2cd3b45c8219e04
akshay ashok <akshayashokan1054@gmail.com>
2024-04-23 21:40:35 +0530

    call dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:food_delivery/pages/food/popular_food_detail.dart';
 import 'package:food_delivery/pages/food/recommended_food_detail.dart';
 import 'package:food_delivery/pages/home/main_food_page.dart';
 import 'package:get/get.dart';
+import 'helper/dependencies.dart' as dep;
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
